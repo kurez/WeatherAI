0a6129b4c42ab8840b4a4e99e635e14ff2bee474
William Masai Kipkures <44669500+kurez@users.noreply.github.com>
2026-06-21 02:03:13 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WeatherController::class, 'index'])->name('weather.index');
+
 Route::post('/weather', [WeatherController::class, 'search'])->name('weather.search');
+
+Route::get('/weather', function () {
+    return redirect()->route('weather.index');
+});
